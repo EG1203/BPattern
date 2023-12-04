e14efbbb0ae2de17e340721e3859a13f59c64131
МАКСИМ МАКСИМ <m12.12.2003@outlook.com>
2023-12-04 14:59:33 +0200

    Добавьте файлы проекта.

---
+++ BPattern/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+// Абстрактный класс для элементов оптической системы
+public abstract class OpticalElement
+{
+    public abstract void PerformOperation();
+}
+
+// Класс для линзы
+public class Lens : OpticalElement
+{
+    public override void PerformOperation()
+    {
+        Console.WriteLine("Операция с линзой");
+    }
+}
+
+// Класс для зеркала
+public class Mirror : OpticalElement
+{
+    public override void PerformOperation()
+    {
+        Console.WriteLine("Операция со зеркалом");
+    }
+}
+
+// Контейнер для оптических элементов, реализующий IEnumerable
+public class OpticalSystem : IEnumerable<OpticalElement>
+{
+    private List<OpticalElement> elements = new List<OpticalElement>();
+
+    public void AddElement(OpticalElement element)
+    {
+        elements.Add(element);
+    }
+
+    // Реализация GetEnumerator для IEnumerable<OpticalElement>
+    public IEnumerator<OpticalElement> GetEnumerator()
+    {
+        foreach (var element in elements)
+        {
+            yield return element;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        OpticalSystem opticalSystem = new OpticalSystem();
+        opticalSystem.AddElement(new Lens());
+        opticalSystem.AddElement(new Mirror());
+
+        // Итерация по элементам оптической системы с использованием foreach
+        foreach (OpticalElement element in opticalSystem)
+        {
+            element.PerformOperation();
+        }
+    }
+}
